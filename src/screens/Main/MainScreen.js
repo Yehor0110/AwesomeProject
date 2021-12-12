94a56b422a70f4496993a80ec27b168349ea4add
Yehor Valentiukevych <mcshka111@gmail.com>
2021-12-12 09:51:39 +0200

    Fixed

--- src/screens/Main/MainScreen.js
+++ src/screens/Main/MainScreen.js
@@ -1,26 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
 import Main from './components/Main/Main';
 import Contacts from './components/Contacts/Contacts'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text style={{ color: '#000' }}>Home Screen</Text>
-//     </View>
-//   );
-// }
-
-// function DetailsScreen() {
-//     return (
-//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//         <Text>Details Screen</Text>
-//       </View>
-//     );
-//   }
 
 const Stack = createNativeStackNavigator();
 
