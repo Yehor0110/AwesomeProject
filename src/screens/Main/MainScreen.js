9bfd66e3b42eb2aabe6ef706fb8b50e08eeed924
Yehor Valentiukevych <mcshka111@gmail.com>
2021-12-16 22:08:31 +0200

    Added react-native-vector-icons

--- src/screens/Main/MainScreen.js
+++ src/screens/Main/MainScreen.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import HomeScreen from './components/Home/Home';
 import SettingsScreen from './components/SettingsScreen/Settings';
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -9,7 +11,26 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeMain() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+    >
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
